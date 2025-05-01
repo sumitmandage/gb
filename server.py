cd5c37597f09b59465ccd18d590c3025aa7b2fd7
sumitmandage <103106134+sumitmandage@users.noreply.github.com>
2025-05-01 22:55:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -21,12 +21,10 @@ CORS(app, resources={
 })
 
 # Configure the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://sumitmandage:Sumit777@sumitmandage.mysql.pythonanywhere-services.com/sumitmandage$default'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'mysql+mysqlconnector://root:root@localhost/new_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'pdf'}  # Define allowed file extensions
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
-
-app.config['UPLOAD_FOLDER'] = '/home/sumitmandage/uploads' 
 
 db.init_app(app)
 
